cfc34c16495a159999f03f2af30947fd2e599f14
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-13 09:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+n = int(input())
+json_object = {}
 
-  //your code here
-	let result = ""; // This will store the final Roman numeral
+for _ in range(n):
+    key, value = input().split()
+    json_object[key] = value
 
-  for (let i = 0; i < romanNumerals.length; i++) {
-    const symbol = romanNumerals[i][0]; // Roman symbol like 'M', 'D', 'C'
-    const value = romanNumerals[i][1];  // Numeric value like 1000, 500, 100
+key_to_delete = input()
 
-    // Keep subtracting value from num and adding symbol to result
-    while (num >= value) {
-      result += symbol;  // Add symbol to result string
-      num -= value;      // Subtract value from num
-    }
+if key_to_delete in json_object:
+    del json_object[key_to_delete]
+
+for key, value in json_object.items():
+    print(key, value)
+
+
+
+// Dont change the code below
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let n = 0;
+let obj = {};
+let stringX = '';
+
+let count = 0;
+
+rl.on('line', (line) => {
+  count++;
+
+  if(count === 1) {
+    n = parseInt(line);
+  } else if(count > 1 && count <= n+1) {
+    let [key, val] = line.split(' ');
+    obj[key] = val;
+  } else if(count === n+2) {
+    stringX = line;
+    rl.close();
   }
+});
 
-  return result; // Return the final Roman numeral
-}
-
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-
+rl.on('close', () => {
+  deleteProperty(obj, stringX)
+  for(let key in obj) {
+    console.log(${key} ${obj[key]});
+  }
+});
